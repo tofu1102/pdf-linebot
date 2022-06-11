eedc0afbf03b01d6659d4e85514520c813fdda26
伊藤裕太 <p3mf5cxkh2@yahoo.co.jp>
2022-06-11 21:03:50 +0900

    imgおうむ返し4

--- main.py
+++ main.py
@@ -45,13 +45,13 @@ def callback():
 def handle_image_message(event):
     message_content = line_bot_api.get_message_content(event.message.id)
 
-    with open(“static/” + event.message.id + “.jpg”, “wb”) as f:
+    with open("static/" + event.message.id + ".jpg", "wb") as f:
         f.write(message_content.content)
         line_bot_api.reply_message(
             event.reply_token,
             ImageSendMessage(
-            original_content_url=FQDN + “/static/” + event.message.id + “.jpg”,
-            preview_image_url=FQDN + “/static/” + event.message.id + “jpg”
+            original_content_url=FQDN + "/static/" + event.message.id + ".jpg",
+            preview_image_url=FQDN + "/static/" + event.message.id + "jpg"
             )
         )
 
