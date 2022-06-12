f6a6fd129f4489479fe2be50ff1b19c2091695f0
伊藤裕太 <p3mf5cxkh2@yahoo.co.jp>
2022-06-12 15:00:32 +0900

    test25

--- img2pdf.py
+++ img2pdf.py
@@ -3,7 +3,7 @@ import img2pdf
 from PIL import Image
 
 def png2pdf(pdfFileName,pngFilePath):
-    with open("static/" + pdf_FileName,"wb") as f:
+    with open("static/" + pdfFileName,"wb") as f:
         f.write(img2pdf.convert([Image.open(pngFilePath).filename]))
 
-    return "static/" + pdf_FileName
+    return "static/" + pdfFileName
