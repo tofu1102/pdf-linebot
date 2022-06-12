de46583c5cc7ee314627c17f09789e4d1169694f
伊藤裕太 <p3mf5cxkh2@yahoo.co.jp>
2022-06-12 10:55:11 +0900

    test10

--- main.py
+++ main.py
@@ -58,6 +58,11 @@ def handle_image_message(event):
         )
 
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+   line_bot_api.reply_message(
+       event.reply_token,
+       TextSendMessage(text=event.message.text))
 
 
 
