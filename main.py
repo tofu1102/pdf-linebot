4ccdf63fc9e8073916ab5204ac0819109d387e92
伊藤裕太 <p3mf5cxkh2@yahoo.co.jp>
2022-06-12 13:11:23 +0900

    test11

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def callback():
 def handle_image_message(event):
     message_content = line_bot_api.get_message_content(event.message.id)
 
-    with open("./static/" + event.message.id + ".jpg", "wb") as f:
+    with open("./tmp/" + event.message.id + ".jpg", "wb") as f:
         f.write(message_content.content)
 
         line_bot_api.reply_message(
