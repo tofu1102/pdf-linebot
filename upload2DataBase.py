c3dfce5d813ddc0c7576dd112f235d5577c1c325
伊藤裕太 <p3mf5cxkh2@yahoo.co.jp>
2022-06-15 22:12:41 +0900

    DBtest3

--- upload2DataBase.py
+++ upload2DataBase.py
@@ -4,16 +4,13 @@ import psycopg2
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 
-with psycopg2.connect(DATABASE_URL) as conn:
-    with conn.cursor() as curs:
-        curs.execute(
-            "INSERT INTO articles(title, url) VALUES(%s, %s)", (title, url))
 
 def insert_test(name):
-    with conn.cursor() as curs:
-        curs.execute(
-        f"INSERT INTO test VALUES({name});"
-        )
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as curs:
+            curs.execute(
+                f"INSERT INTO test VALUES({name});"
+                )
 
 if __name__ == '__main__':
     insert_test("tofu")
