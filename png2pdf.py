f9472a2d021568286a2b861e507f0afd928eb3b7
伊藤裕太 <p3mf5cxkh2@yahoo.co.jp>
2022-06-17 21:09:30 +0900

    名称変更完成

--- png2pdf.py
+++ png2pdf.py
@@ -3,7 +3,7 @@ import img2pdf
 from PIL import Image
 
 def png2pdf(pdfFileName,pngFilePath):
-    with open("static/" + pdfFileName,"wb") as f:
+    with open("static/" + pdfFileName + ".pdf","wb") as f:
         f.write(img2pdf.convert([Image.open(pngFilePath).filename]))
 
     return pdfFileName
